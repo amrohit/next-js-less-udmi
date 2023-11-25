b6989874111c7e624270c11830496c723ad52155
Coding Scenes <withjsonly@gmail.com>
2023-11-25 15:35:55 +0530

    Update layout.jsx

--- app/reviews/layout.jsx
+++ app/reviews/layout.jsx
@@ -1,13 +1,9 @@
 const ReviewsLayout = ({ children }) => {
   return (
-      <div style={{ display: 'flex'}}>
-          <div style={{ border: '1px solid #ccc'}}>
-              [reviews menubar]
-          </div>
-          <div>
-              { children }
-          </div>
-      </div>
-  )
-}
-export default ReviewsLayout
+    <div style={{ display: "flex" }}>
+      <div style={{ border: "1px solid #ccc" }}>[reviews menubar]</div>
+      <div>{children}</div>
+    </div>
+  );
+};
+export default ReviewsLayout;
